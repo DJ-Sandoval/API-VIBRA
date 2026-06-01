52d4a64deb27af60febe7b509226295104ef5213
DJ-Sandoval <sandovalsantanajosearmando@gmail.com>
2026-06-01 10:25:51 -0600

    First commit Empty proyect

---
+++ src/main/java/com/a/s/APIVibraBike/ApiVibraBikeApplication.java
@@ -0,0 +1,13 @@
+package com.a.s.APIVibraBike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVibraBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVibraBikeApplication.class, args);
+	}
+
+}
